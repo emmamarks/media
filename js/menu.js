112405360aba8e6e8f6c616a820ff03420f59784
emmamarks <emmamarks001@gmail.com>
2022-06-16 10:44:10 +0100

    first

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+$('#menu-button').click(function() {
+    $(this).toggleClass('change');
+    $('.nav-links').toggleClass('nav-open');
+});
+
+/* If the window is resized and the class change has already been
+applied, then toggle it to avoid an issue with the menu button disappearing */
+$(window).resize(function() {
+    if ($(window).width() > 768) {
+         console.log($(window).width());
+         if ($('.nav-links').hasClass('nav-open')) {
+              $('#menu-button').toggleClass('change');
+              $('.nav-links').toggleClass('nav-open');
+         }
+    }
+});
---
+++ js/nav.js
@@ -0,0 +1,11 @@
+$(function() {
+    $(window).scroll(function() {
+         var $this = $(window);
+
+         if($this.scrollTop() > 100) {
+              $('#main-nav').addClass('fixed-nav');
+         } else {
+              $('#main-nav').removeClass('fixed-nav');
+         }
+    });
+});
